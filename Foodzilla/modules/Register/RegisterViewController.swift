d2d28a370eb80ddae885f45519beb78f0b1b9875
aniaxb <a.banachowicz11@gmail.com>
2023-03-30 18:24:45 +0200

    Added RegisterView along with RegisterViewController

---
+++ Foodzilla/modules/Register/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  Foodzilla
+//
+//  Created by Ania on 30/03/2023.
+//
+
+import UIKit
+
+final class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
