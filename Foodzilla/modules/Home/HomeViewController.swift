d7174412c10105cbf1a000d4173f1bfc73509d2d
aniaxb <a.banachowicz11@gmail.com>
2023-04-08 14:49:51 +0200

    Added proposed HomeView

---
+++ Foodzilla/modules/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Foodzilla
+//
+//  Created by Ania Banachowicz on 05/04/2023.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func loadView() {
+        super.loadView()
+    }
+}
