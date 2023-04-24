1823e23cfe5026d63d0586ee471c64afa5c5e064
aniaxb <a.banachowicz11@gmail.com>
2023-04-24 19:18:35 +0200

    Deleted CollectionViewCell and added logic to tagsLabel

--- Foodzilla/modules/UserPanel/UserPanelViewController.swift
+++ Foodzilla/modules/UserPanel/UserPanelViewController.swift
@@ -21,11 +21,9 @@ final class UserPanelViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(UINib(nibName: "EnabledTagViewCell", bundle: nil), forCellWithReuseIdentifier: "EnabledTagViewCell")
     }
+
+    @IBOutlet weak var tagsLabel: UILabel!
 
     override func loadView() {
         super.loadView()
@@ -35,30 +33,18 @@ final class UserPanelViewController: UIViewController {
         firstNameLabel.mainLabel.text = "User"
         lastNameLabel.mainLabel.text = "Name"
         emailLabel.mainLabel.text = "abc@gmail.com"
-    }
+
+        tagsLabel.text = ""
+
+        for (index, _) in model.tags.enumerated() {
+            tagsLabel.text?.append("\(model.tags[index].name) ")
+
+            if index != model.tags.count - 1 {
+                tagsLabel.text?.append("| ")
+            }
+        }
+
+        if tagsLabel.text?.hasSuffix("| ") == true {
+            tagsLabel.text?.removeLast(2)
+        }    }
 }
-
-extension UserPanelViewController:
-    UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return model.tags.count
-        }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        guard let cell = collectionView.cellForItem(at: indexPath) as? EnabledTagViewCell else { return .zero }
-//        return CGSize(width: cell.tagLabel.frame.width, height: cell.tagLabel.frame.height)
-            return CGSize(width: 80, height: 50)
-        }
-
-    
-    // zamiast collectioview daj sam label i przypisz label.text += cell.text + cell.text
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EnabledTagViewCell", for: indexPath) as? EnabledTagViewCell else { fatalError() }
-            cell.tagLabel.text = model.tags[indexPath.item].name
-
-            return cell
-        }
-
-    }
