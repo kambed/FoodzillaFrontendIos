374eee3597104d787def45d6c70757ef6b2b02dc
Pawel Dera <pawel.dera@bcf-software.com>
2023-04-24 14:19:54 +0200

    Imp of filter screen

--- Foodzilla/SceneDelegate.swift
+++ Foodzilla/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
 
-        let vc = UIStoryboard(name: "Filter", bundle: nil).instantiateViewController(withIdentifier: "Filter")
+        let vc = UIStoryboard(name: "Filters", bundle: nil).instantiateViewController(withIdentifier: "Filters")
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
     }
--- Foodzilla/modules/Filters/FiltersViewController.swift
+++ Foodzilla/modules/Filters/FiltersViewController.swift
@@ -6,11 +6,114 @@
 //
 
 import UIKit
+import MaterialComponents
 
 class FiltersViewController: UIViewController {
 
+    
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var preparationTime: PreparationTime!
+    @IBOutlet var recentTagsLabel: UILabel!
+    @IBOutlet var otherTagsLabel: UILabel!
+    @IBOutlet var recentTagsCollectionView: UICollectionView!
+    @IBOutlet var otherTagsCollectionView: UICollectionView!
+    
+    let recentTagsData = [
+        "dietary",
+        "low-carb",
+        "lactose",
+        "main-dish",
+        "side-dishes",
+        "easy",
+        "greens",
+        "gluten-free",
+        "vegetables",
+        "high-calcium"
+    ]
+    
+    let otherTagsData = [
+        "week-night",
+        "occasion",
+        "healthy",
+        "cuisine"
+    ]
+    
+    var recentTagsCV: RecentTagsCV!
+    var otherTagsCV: OtherTagsCV!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        descriptionLabel.text = "tags will be applied in order to filter recipes in search tab"
+        recentTagsLabel.text = "Recent tags"
+        otherTagsLabel.text = "Other tags"
+        
+        recentTagsCV = RecentTagsCV(data: recentTagsData)
+        otherTagsCV = OtherTagsCV(data: otherTagsData)
 
+        preparationTime.delegate = self
+        
+        recentTagsCollectionView.register(ChipCollectionViewCell.self, forCellWithReuseIdentifier: "ChipCollectionViewCell")
+        recentTagsCollectionView.delegate = recentTagsCV
+        recentTagsCollectionView.dataSource = recentTagsCV
+        
+        otherTagsCollectionView.register(ChipCollectionViewCell.self, forCellWithReuseIdentifier: "ChipCollectionViewCell")
+        otherTagsCollectionView.delegate = otherTagsCV
+        otherTagsCollectionView.dataSource = otherTagsCV
     }
 }
+
+extension FiltersViewController: PreparationTimeDelegate {
+    func getPreparationTime(preparationTime: Float) {
+        print(preparationTime)
+    }
+}
+
+class RecentTagsCV: NSObject, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let data: [String]
+    
+    init(data: [String]) {
+        self.data = data
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        self.data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChipCollectionViewCell", for: indexPath) as! ChipCollectionViewCell
+        let chipView = cell.chipView
+        
+        chipView.titleLabel.text = data[indexPath.item]
+        return cell
+    }
+    
+    
+}
+
+class OtherTagsCV: NSObject, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let data: [String]
+    
+    init(data: [String]) {
+        self.data = data
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        self.data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChipCollectionViewCell", for: indexPath) as! ChipCollectionViewCell
+        let chipView = cell.chipView
+        
+        chipView.titleLabel.text = data[indexPath.item]
+        return cell
+    }
+    
+    
+}
+
+
