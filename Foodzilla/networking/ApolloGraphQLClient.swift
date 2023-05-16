9c2dc2744ad4ecec06a521229fc6429ecce1f3b6
Pawel Dera <pawel.dera@bcf-software.com>
2023-05-16 16:11:20 +0200

    Update graphql imp

--- Foodzilla/networking/ApolloGraphQLClient.swift
+++ Foodzilla/networking/ApolloGraphQLClient.swift
@@ -8,24 +8,81 @@
 import UIKit
 import Apollo
 
+struct User: Codable {
+    var firstName: String?
+    var lastName: String?
+    var username: String?
+    var password: String?
+}
+
 class ApolloGraphQLClient {
     
     private init() { }
     
     private var apolloClient: ApolloClient = {
-        let apolloClient = ApolloClient(url: URL(string: "http://localhost:4000/graphql")!)
+        let apolloClient = ApolloClient(url: URL(string: "http://localhost:8080/graphql")!)
         return apolloClient
     }()
     
-    var shared: ApolloGraphQLClient = {
+    static let shared: ApolloGraphQLClient = {
         return ApolloGraphQLClient()
     }()
     
-    public func getTags(completion: @escaping (Result<FoodzillaGraphQL.TagsQuery.Data, GraphQLError>) -> Void) {
-        apolloClient.fetch(query: FoodzillaGraphQL.TagsQuery()) { result in
-            guard let data = try? result.get().data else { return completion(.failure(GraphQLError.couldntFetchData)) }
-            completion(.success(data))
+    private func performQuery<Operation: GraphQLOperation & GraphQLQuery>(operation: Operation) async throws -> Operation.Data {
+        return try await withCheckedThrowingContinuation { continuation in
+            apolloClient.fetch(query: operation) { result in
+                switch result {
+                    case .success(let graphQLResult):
+                        if let errors = graphQLResult.errors {
+                            continuation.resume(throwing: NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "GraphQL Error: \(errors)"]))
+                        } else if let data = graphQLResult.data {
+                            continuation.resume(returning: data)
+                        }
+                    case .failure(let error):
+                        continuation.resume(throwing: error)
+                }
+            }
         }
+    }
+    
+    private func performMutation<Operation: GraphQLOperation & GraphQLMutation>(operation: Operation) async throws -> Operation.Data {
+        return try await withCheckedThrowingContinuation { continuation in
+            apolloClient.perform(mutation: operation) { result in
+                switch result {
+                    case .success(let graphQLResult):
+                        if let errors = graphQLResult.errors {
+                            continuation.resume(throwing: NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "GraphQL Error: \(errors)"]))
+                        } else if let data = graphQLResult.data {
+                            continuation.resume(returning: data)
+                        }
+                    case .failure(let error):
+                        continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+
+    
+    public func getTags() async throws -> FoodzillaGraphQL.TagsQuery.Data {
+        return try await performQuery(operation: FoodzillaGraphQL.TagsQuery())
+    }
+    
+    public func createUser(user: User) async throws -> FoodzillaGraphQL.CreateMutation.Data {
+        guard let firstName = user.firstName, let lastName = user.lastName, let username = user.username, let password = user.password else {
+            throw UserError.noRegistrationCredentials
+        }
+                
+        let mutation = FoodzillaGraphQL.CreateMutation(firstname: firstName, lastname: lastName, username: username, password: password)
+        return try await performMutation(operation: mutation)
+    }
+    
+    public func loginUser(user: User) async throws -> FoodzillaGraphQL.LoginMutation.Data {
+        guard let username = user.username, let password = user.password else {
+            throw UserError.noLoginCredentials
+        }
+        
+        let mutation = FoodzillaGraphQL.LoginMutation(username: username, password: password)
+        return try await performMutation(operation: mutation)
     }
 }
 
---
+++ Foodzilla/networking/errors/UserErrors.swift
@@ -0,0 +1,13 @@
+//
+//  UserErrors.swift
+//  Foodzilla
+//
+//  Created by Paweł on 16/05/2023.
+//
+
+import UIKit
+
+enum UserError: Error {
+    case noRegistrationCredentials
+    case noLoginCredentials
+}
